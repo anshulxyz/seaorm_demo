c3ba05e87654906ab26a65b4e04f8792ac4ef28d
Anshul Chauhan <anshulchauhan@outlook.com>
2022-09-03 00:19:39 +0530

    Fix formatting, use spaces because cargo fmt

--- src/bin/read_posts.rs
+++ src/bin/read_posts.rs
@@ -1,18 +1,18 @@
+use entity::post;
 use migration::DbErr;
 use sea_orm::EntityTrait;
 use seaorm_demo::establish_connection;
-use entity::post;
 
 #[tokio::main]
-async fn main() -> Result<(), DbErr>{
-	let db = establish_connection().await?;
+async fn main() -> Result<(), DbErr> {
+    let db = establish_connection().await?;
 
-	let posts: Vec<post::Model> = post::Entity::find().all(&db).await?;
+    let posts: Vec<post::Model> = post::Entity::find().all(&db).await?;
 
-	println!("All the posts in db:");
-	for post in posts {
-		println!("ID: {}, TITLE: {}", post.id, post.title);
-	}
+    println!("All the posts in db:");
+    for post in posts {
+        println!("ID: {}, TITLE: {}", post.id, post.title);
+    }
 
-	Ok(())
+    Ok(())
 }
--- src/bin/update_post.rs
+++ src/bin/update_post.rs
@@ -1,19 +1,22 @@
-use migration::DbErr;
-use sea_orm::{EntityTrait, Set, ActiveModelTrait};
-use seaorm_demo::establish_connection;
 use entity::post;
+use migration::DbErr;
+use sea_orm::{ActiveModelTrait, EntityTrait, Set};
+use seaorm_demo::establish_connection;
 
 #[tokio::main]
-async fn main() -> Result<(), DbErr>{
-	let db = establish_connection().await?;
+async fn main() -> Result<(), DbErr> {
+    let db = establish_connection().await?;
 
-	// UPDATE titel of Post by ID
-	let post = post::Entity::find_by_id(1).one(&db).await?;
-	let mut post: post::ActiveModel = post.unwrap().into();
-	post.title = Set("Updated title".to_owned());
-	let post: post::Model = post.update(&db).await?;
+    // UPDATE titel of Post by ID
+    let post = post::Entity::find_by_id(1).one(&db).await?;
+    let mut post: post::ActiveModel = post.unwrap().into();
+    post.title = Set("Updated title".to_owned());
+    let post: post::Model = post.update(&db).await?;
 
-	println!("Post updated for ID: {} with TITLE: {}", post.id, post.title);
+    println!(
+        "Post updated for ID: {} with TITLE: {}",
+        post.id, post.title
+    );
 
-	Ok(())
+    Ok(())
 }
