422327b5cf4ad4322bab646a8a963888b7f3bc65
Anshul Chauhan <anshulchauhan@outlook.com>
2022-09-09 15:43:47 +0530

    Update formatting

--- src/bin/create_post.rs
+++ src/bin/create_post.rs
@@ -1,21 +1,21 @@
-use migration::DbErr;
-use sea_orm::{Set, ActiveModelTrait};
-use seaorm_demo::establish_connection;
 use entity::post;
+use migration::DbErr;
+use sea_orm::{ActiveModelTrait, Set};
+use seaorm_demo::establish_connection;
 
 #[tokio::main]
-async fn main() -> Result<(), DbErr>{
-	let db = establish_connection().await?;
+async fn main() -> Result<(), DbErr> {
+    let db = establish_connection().await?;
 
-	let post = post::ActiveModel {
-		title: Set(String::from("Another title 2")),
-		text: Set(String::from("Lorem ipsum dolor sit amet.")),
-		..Default::default()
-	};
+    let post = post::ActiveModel {
+        title: Set(String::from("Another title 2")),
+        text: Set(String::from("Lorem ipsum dolor sit amet.")),
+        ..Default::default()
+    };
 
-	let post: post::Model = post.insert(&db).await?;
+    let post: post::Model = post.insert(&db).await?;
 
-	println!("Post created with ID: {}, TITLE: {}", post.id, post.title);
+    println!("Post created with ID: {}, TITLE: {}", post.id, post.title);
 
-	Ok(())
+    Ok(())
 }
